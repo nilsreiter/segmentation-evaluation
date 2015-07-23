f9696b4d0618665a2c75b2c2471feca3ead738bf
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-07-23 18:37:24 +0200

    Now we have a propert initialize function

--- de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/evaluation/util/SegmentBoundaryAnnotator.java
+++ de.ustu.creta.segmentation.evaluation/segmentation.evaluation/src/main/java/de/ustu/creta/segmentation/evaluation/util/SegmentBoundaryAnnotator.java
@@ -1,5 +1,6 @@
 package de.ustu.creta.segmentation.evaluation.util;
 
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
@@ -7,6 +8,7 @@ import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import de.ustu.ims.segmentation.type.SegmentBoundary;
 
@@ -20,13 +22,20 @@ public class SegmentBoundaryAnnotator extends JCasAnnotator_ImplBase {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
+	public void initialize(final UimaContext context)
+			throws ResourceInitializationException {
+		super.initialize(context);
 		try {
 			Class<?> clazz = Class.forName(annotationTypeName);
 			annotationType = (Class<? extends Annotation>) clazz;
 		} catch (ClassNotFoundException e) {
-			throw new AnalysisEngineProcessException(e);
+			throw new ResourceInitializationException(e);
 		}
+	}
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
 		for (Annotation anno : JCasUtil.select(jcas, annotationType)) {
 			int b = anno.getBegin();
 			AnnotationFactory.createAnnotation(jcas, b, b,
