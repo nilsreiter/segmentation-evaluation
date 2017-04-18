e978d5f7a0cefcf4a1f86d56c08e8d1e1080dea5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-04-18 10:28:45 +0200

    using new, more generic Metric interface

--- de.unistuttgart.ims.segmentation.uima/src/main/java/de/unistuttgart/ims/segmentation/uima/evaluation/SimpleSegEval.java
+++ de.unistuttgart.ims.segmentation.uima/src/main/java/de/unistuttgart/ims/segmentation/uima/evaluation/SimpleSegEval.java
@@ -1,37 +1,47 @@
 package de.unistuttgart.ims.segmentation.uima.evaluation;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiFunction;
 
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
-import de.unistuttgart.ims.segmentation.evaluation.MassStringMetric;
+import de.unistuttgart.ims.segmentation.evaluation.BoundarySetsMetric;
+import de.unistuttgart.ims.segmentation.evaluation.Metric;
 import de.unistuttgart.ims.segmentation.evaluation.WindowMetric;
+import de.unistuttgart.ims.segmentation.uima.evaluation.util.Util;
 
 public class SimpleSegEval<SEGMENT_ANNOTATION extends Annotation, BASE_ANNOTATION extends Annotation> {
-	MassStringMetric metric;
+	Metric metric;
 	Class<SEGMENT_ANNOTATION> segmentClass;
 	Class<BASE_ANNOTATION> baseClass;
 
-	public SimpleSegEval(MassStringMetric metric, Class<SEGMENT_ANNOTATION> segmentClass, Class<BASE_ANNOTATION> baseClass) {
+	public SimpleSegEval(Metric metric, Class<SEGMENT_ANNOTATION> segmentClass, Class<BASE_ANNOTATION> baseClass) {
 		this.metric = metric;
 		this.segmentClass = segmentClass;
 		this.baseClass = baseClass;
 	}
 
 	public double score(JCas goldView, JCas silverView, int windowSize) {
-		int[] gold = getMassString(goldView, segmentClass, baseClass);
-		int[] silver = getMassString(silverView, segmentClass, baseClass);
 
-		return ((WindowMetric) metric).score(gold, silver, windowSize);
-	}
+		if (metric instanceof WindowMetric) {
+			int[] gold = getMassString(goldView, segmentClass, baseClass);
+			int[] silver = getMassString(silverView, segmentClass, baseClass);
+			return ((WindowMetric) metric).score(gold, silver, windowSize);
+		} else if (metric instanceof BoundarySetsMetric) {
 
-	public double score(JCas goldView, JCas silverView) {
-		int[] gold = getMassString(goldView, segmentClass, baseClass);
-		int[] silver = getMassString(silverView, segmentClass, baseClass);
+			BiFunction<SEGMENT_ANNOTATION, String, String> fnc = Util.getFunction();
+			List<Set<String>> goldBS = Util.getBoundarySetSequence(goldView, segmentClass, baseClass, fnc);
+			List<Set<String>> silverBS = Util.getBoundarySetSequence(silverView, segmentClass, baseClass, fnc);
 
-		return metric.score(gold, silver);
+			return ((BoundarySetsMetric) metric).score(goldBS, silverBS, windowSize);
+
+		}
+		return Double.NaN;
+
 	}
 
 	protected int[] getMassString(JCas jcas, Class<SEGMENT_ANNOTATION> segmentClass, Class<BASE_ANNOTATION> baseClass) {
