8f9345515da7b961c2aa5ad8cf750c87d98b21c0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-04-18 08:17:24 +0200

    Fixed tests

--- de.unistuttgart.ims.segmentation.uima/src/main/java/de/unistuttgart/ims/segmentation/uima/evaluation/util/Util.java
+++ de.unistuttgart.ims.segmentation.uima/src/main/java/de/unistuttgart/ims/segmentation/uima/evaluation/util/Util.java
@@ -1,45 +1,48 @@
 package de.unistuttgart.ims.segmentation.uima.evaluation.util;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiFunction;
 
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
-import com.google.common.collect.Sets;
-
 public class Util {
+	public static final String BEGIN = "BEGIN";
+	public static final String END = "END";
+
 	public static <BASE_TYPE extends Annotation, SEGMENT_TYPE extends Annotation, BOUNDARY_TYPE> List<Set<BOUNDARY_TYPE>> getBoundarySetSequence(
 			JCas jcas, Class<SEGMENT_TYPE> segmentType, Class<BASE_TYPE> subType,
 			BiFunction<SEGMENT_TYPE, String, BOUNDARY_TYPE> btFunction) {
 
-		Set<Integer> begins = Sets.newHashSet(), ends = Sets.newHashSet();
+		MultiValuedMap<Integer, SEGMENT_TYPE> begins = new HashSetValuedHashMap<Integer, SEGMENT_TYPE>(),
+				ends = new HashSetValuedHashMap<Integer, SEGMENT_TYPE>();
 		for (SEGMENT_TYPE st : JCasUtil.select(jcas, segmentType)) {
-			begins.add(st.getBegin());
-			ends.add(st.getEnd());
+			begins.put(st.getBegin(), st);
+			ends.put(st.getEnd(), st);
 		}
 
-		Collection<BASE_TYPE> coll = JCasUtil.select(jcas, subType);
+		List<BASE_TYPE> coll = new ArrayList<BASE_TYPE>(JCasUtil.select(jcas, subType));
 		ListSet<BOUNDARY_TYPE> r = new ListSet<BOUNDARY_TYPE>();
 		for (int i = 0; i < coll.size() - 1; i++) {
 			r.add(i, new HashSet<BOUNDARY_TYPE>());
 		}
-		int i = 0;
-		for (final BASE_TYPE anno : coll) {
-
-			if (begins.contains(anno.getBegin())) {
-				r.addSetItem(i, btFunction.apply(null, "begin"));
+		for (int i = 0; i < coll.size(); i++) {
+			BASE_TYPE anno = coll.get(i);
+			if (begins.containsKey(anno.getBegin()) && i > 0) {
+				for (SEGMENT_TYPE s : begins.get(anno.getBegin()))
+					r.addSetItem(i - 1, btFunction.apply(s, BEGIN));
 			}
-			if (ends.contains(anno.getEnd())) {
-				r.addSetItem(i + 1, btFunction.apply(null, "end"));
+			if (ends.containsKey(anno.getEnd()) && i < coll.size() - 1) {
+				for (SEGMENT_TYPE s : ends.get(anno.getEnd()))
+					r.addSetItem(i, btFunction.apply(s, END));
 			}
 
-			i++;
 		}
 		return r;
 
--- de.unistuttgart.ims.segmentation.uima/src/test/java/de/unistuttgart/ims/segmentation/uima/evaluation/util/TestUtil.java
+++ de.unistuttgart.ims.segmentation.uima/src/test/java/de/unistuttgart/ims/segmentation/uima/evaluation/util/TestUtil.java
@@ -12,15 +12,25 @@ import org.apache.uima.UIMAException;
 import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.unistuttgart.ims.segmentation.type.Segment;
 import de.unistuttgart.ims.segmentation.type.SegmentationSubUnit;
 
 public class TestUtil {
-	@Test
-	public void testGetBoundarySetsString() throws UIMAException {
-		JCas jcas = JCasFactory.createJCas();
+
+	JCas jcas;
+	BiFunction<Segment, String, String> bif = new BiFunction<Segment, String, String>() {
+
+		public String apply(Segment segment, String arg0) {
+			return arg0;
+		}
+	};
+
+	@Before
+	public void setUp() throws UIMAException {
+		jcas = JCasFactory.createJCas();
 		jcas.setDocumentText("abcdefgh");
 
 		int i = 0;
@@ -39,26 +49,39 @@ public class TestUtil {
 		AnnotationFactory.createAnnotation(jcas, i, i + 1, SegmentationSubUnit.class);
 		i++;
 		AnnotationFactory.createAnnotation(jcas, i, i + 1, SegmentationSubUnit.class);
+	}
+
+	@Test
+	public void testGetBoundarySetsString() throws UIMAException {
 
 		AnnotationFactory.createAnnotation(jcas, 0, 3, Segment.class);
 		AnnotationFactory.createAnnotation(jcas, 5, 6, Segment.class);
 
-		List<Set<String>> l = Util.getBoundarySetSequence(jcas, Segment.class, SegmentationSubUnit.class,
-				new BiFunction<Segment, String, String>() {
-
-					public String apply(Segment segment, String arg0) {
-						return arg0;
-					}
-				});
-		System.err.println(jcas.getDocumentText().toCharArray());
-		System.err.println(l);
+		List<Set<String>> l = Util.getBoundarySetSequence(jcas, Segment.class, SegmentationSubUnit.class, bif);
+		// System.out.println(l);
 		assertEquals(7, l.size());
+		assertTrue(l.get(0).isEmpty());
+		assertTrue(l.get(1).isEmpty());
+		assertFalse(l.get(2).isEmpty());
+		assertTrue(l.get(3).isEmpty());
+		assertFalse(l.get(4).isEmpty());
+		assertFalse(l.get(5).isEmpty());
+		assertTrue(l.get(6).isEmpty());
+	}
+
+	@Test
+	public void testGetBoundarySetsString2() throws UIMAException {
+
+		AnnotationFactory.createAnnotation(jcas, 0, 1, Segment.class);
+		AnnotationFactory.createAnnotation(jcas, 6, 9, Segment.class);
+		List<Set<String>> l = Util.getBoundarySetSequence(jcas, Segment.class, SegmentationSubUnit.class, bif);
+		// System.out.println(l);
 		assertFalse(l.get(0).isEmpty());
 		assertTrue(l.get(1).isEmpty());
 		assertTrue(l.get(2).isEmpty());
-		assertFalse(l.get(3).isEmpty());
+		assertTrue(l.get(3).isEmpty());
 		assertTrue(l.get(4).isEmpty());
 		assertFalse(l.get(5).isEmpty());
-		assertFalse(l.get(6).isEmpty());
+		assertTrue(l.get(6).isEmpty());
 	}
 }
